2c9b707dcb24c473aeef854a71201797ec4caa74
adifalco <adifalco@nd.edu>
2018-09-20 22:59:32 -0400

    Showing subject

---
+++ reader.py
@@ -0,0 +1,10 @@
+import numpy as np
+import cv2
+
+# Hardcoded for now -- will read from csv file
+path = 'images\\orl_faces\\training\\s1\\1.pgm'
+
+img = cv2.imread()
+cv2.imshow('s1/s1.pgm',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
